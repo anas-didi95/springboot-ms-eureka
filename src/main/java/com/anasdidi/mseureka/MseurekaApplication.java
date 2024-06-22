da070ddf44150bbf687e3b7d3785dc5b33fbf1d7
Anas Juwaidi <anas.didi95@gmail.com>
2024-06-22 10:56:10 +0000

    ADD: Generate project from spring init

---
+++ src/main/java/com/anasdidi/mseureka/MseurekaApplication.java
@@ -0,0 +1,12 @@
+package com.anasdidi.mseureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MseurekaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MseurekaApplication.class, args);
+  }
+}
