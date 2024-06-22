416dfbe2d8515e799e082a38cff8fb984959d5b6
Anas Juwaidi <anas.didi95@gmail.com>
2024-06-22 13:05:57 +0000

    ADD: Add security

--- src/main/java/com/anasdidi/mseureka/MseurekaApplication.java
+++ src/main/java/com/anasdidi/mseureka/MseurekaApplication.java
@@ -4,9 +4,11 @@ package com.anasdidi.mseureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableWebSecurity
 public class MseurekaApplication {
 
   public static void main(String[] args) {
