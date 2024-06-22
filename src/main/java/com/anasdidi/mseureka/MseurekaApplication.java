bb6b81b553256c5d445a7f2a39141bebd13c29b2
Anas Juwaidi <anas.didi95@gmail.com>
2024-06-22 11:45:27 +0000

    ADD: Implement eureka server

--- src/main/java/com/anasdidi/mseureka/MseurekaApplication.java
+++ src/main/java/com/anasdidi/mseureka/MseurekaApplication.java
@@ -3,8 +3,10 @@ package com.anasdidi.mseureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MseurekaApplication {
 
   public static void main(String[] args) {
