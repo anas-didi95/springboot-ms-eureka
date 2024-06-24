6be064f21c564fcc7e1c3257e4787abfb4cf4011
Anas Juwaidi <anas.didi95@gmail.com>
2024-06-24 14:03:30 +0000

    ADD: Add test

--- src/test/java/com/anasdidi/mseureka/MseurekaApplicationTests.java
+++ src/test/java/com/anasdidi/mseureka/MseurekaApplicationTests.java
@@ -1,12 +1,40 @@
 /* (C) 2024 */
 package com.anasdidi.mseureka;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 class MseurekaApplicationTests {
 
+  @Autowired private TestRestTemplate restTemplate;
+
+  @Value("${spring.security.user.name}")
+  private String username;
+
+  @Value("${spring.security.user.password}")
+  private String password;
+
   @Test
-  void contextLoads() {}
+  @Order(Integer.MIN_VALUE)
+  void contextLoads() {
+    Assertions.assertNotNull(restTemplate);
+  }
+
+  @Test
+  void testEurekaUp() {
+    ResponseEntity<Object> response =
+        restTemplate
+            .withBasicAuth(username, password)
+            .getForEntity("/eureka/v2/apps", Object.class);
+    Assertions.assertEquals(response.getStatusCode().value(), HttpStatus.OK.value());
+  }
 }
